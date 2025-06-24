4f7e56e84c28b62bfe089cde1e9ed1c430154b9e
juvoc <julien.@student.unamur.be>
2025-06-24 08:58:56 +0200

    refactor: cleaning updateQuality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,52 +10,47 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(ItemConstants.AGED_BRIE)
-                    && !items[i].name.equals(ItemConstants.BACKSTAGE_PASS)) {
-                if (items[i].quality > ItemConstants.MIN_NORMAL_QUALITY) {
-                    if (!items[i].name.equals(ItemConstants.SULFURAS)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < ItemConstants.MAX_NORMAL_QUALITY) {
-                    items[i].quality = items[i].quality + 1;
+            if(items[i].name.equals(ItemConstants.SULFURAS))
+            {
+               continue;
+            }
+            boolean itemQualityIncreases = items[i].name.equals(ItemConstants.AGED_BRIE) || items[i].name.equals(ItemConstants.BACKSTAGE_PASS);
+            if(itemQualityIncreases)
+            {
+                int qualityIncrease = 1;
 
-                    if (items[i].name.equals(ItemConstants.BACKSTAGE_PASS)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < ItemConstants.MAX_NORMAL_QUALITY) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < ItemConstants.MAX_NORMAL_QUALITY) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+                if(items[i].name.equals(ItemConstants.BACKSTAGE_PASS))
+                {
+                   if(items[i].sellIn < 11)
+                   {
+                       qualityIncrease = 2;
+                   }
+                   if (items[i].sellIn < 6) {
+                       qualityIncrease = 3;
+                   }
                 }
+                items[i].quality = Math.min(ItemConstants.MAX_NORMAL_QUALITY, items[i].quality + qualityIncrease);
+            }
+            else
+            {
+                items[i].quality = Math.max(ItemConstants.MIN_NORMAL_QUALITY, items[i].quality - 1);
             }
 
-            if (!items[i].name.equals(ItemConstants.SULFURAS)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            items[i].sellIn = items[i].sellIn - 1;
 
-            if (items[i].sellIn < ItemConstants.MIN_NORMAL_QUALITY) {
-                if (!items[i].name.equals(ItemConstants.AGED_BRIE)) {
-                    if (!items[i].name.equals(ItemConstants.BACKSTAGE_PASS)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(ItemConstants.SULFURAS)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < ItemConstants.MAX_NORMAL_QUALITY) {
-                        items[i].quality = items[i].quality + 1;
-                    }
+            if(items[i].sellIn < 0)
+            {
+                if(items[i].name.equals(ItemConstants.BACKSTAGE_PASS))
+                {
+                    items[i].quality = ItemConstants.MIN_NORMAL_QUALITY;
+                }
+                else if (items[i].name.equals(ItemConstants.AGED_BRIE))
+                {
+                    items[i].quality = Math.min(ItemConstants.MAX_NORMAL_QUALITY, items[i].quality + 1);
+                }
+                else
+                {
+                    items[i].quality = Math.max(ItemConstants.MIN_NORMAL_QUALITY, items[i].quality - 1);
                 }
             }
         }
